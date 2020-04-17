d2a059afc0410159dd5c2d876caa97021ebf7e62
akstokely <annabeth.stokely@gmail.com>
2020-04-16 21:23:39 -0400

    Saved test api call.

--- src/main/java/edu/brown/cs/student/database/RecipeDatabase.java
+++ src/main/java/edu/brown/cs/student/database/RecipeDatabase.java
@@ -4,6 +4,11 @@ import edu.brown.cs.student.food.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -51,6 +56,28 @@ public class RecipeDatabase {
     Connection conn = DriverManager.getConnection(urlToDB);
     Statement stat = conn.createStatement();
     stat.executeUpdate("PRAGMA foreign_keys=ON;");
+  }
+
+  /**
+   * Test api function
+   */
+  public void apiCall() {
+    HttpClient httpClient = HttpClient.newBuilder().build();
+    HttpRequest httpRequest = HttpRequest.newBuilder().GET()
+        .uri(URI.create("https://api.edamam.com/search?q=chicken&app_id=2a676518"
+            + "&app_key=" +
+            "158f55a83eee58aff1544072b788784f&from=0&to=3&calories=591-722&health=alcohol-free")).build();
+
+    try {
+      HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+      System.out.println(response.body());
+      System.out.println(response.statusCode());
+    } catch (IOException | InterruptedException ioe) {
+      ioe.printStackTrace();
+    }
+
+
+
   }
   
   
