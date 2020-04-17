04e5fac1ed473fa1876173bfb493ec28aa8aacc7
ojohnso3 <50245784+ojohnso3@users.noreply.github.com>
2020-04-16 22:37:44 -0400

    more to recommender

--- src/main/java/edu/brown/cs/student/recommendation/Recommender.java
+++ src/main/java/edu/brown/cs/student/recommendation/Recommender.java
@@ -16,9 +16,12 @@ import edu.brown.cs.student.food.Recipe;
  */
 public class Recommender {
   private HashMap<String, KDTree<RecipeNode>> userTrees;
+  private HashMap<String, List<Recipe>> userRecs;
+
   
   public Recommender() {
     userTrees = new HashMap<String, KDTree<RecipeNode>>();
+    userRecs = new HashMap<String, List<Recipe>>();
   }
   
   /**
@@ -30,23 +33,39 @@ public class Recommender {
   public List<Recipe> makeRecommendation(String userID, String input) {
     KDTree<RecipeNode> kdtree = this.getKDTree(userID);
     List<Recipe> recs = this.getRecommendedRecipes(kdtree);
+    
+    if (!userRecs.containsKey(userID)) {
+      userRecs.put(userID, new ArrayList<Recipe>());
+    }
+    for (Recipe r : recs) {
+      userRecs.get(userID).add(r); // figure out order and backwards?
+    }
     return recs;
   }
   
   private KDTree<RecipeNode> getKDTree(String userID) {
     KDTree<RecipeNode> kdtree;
-    if (userTrees.containsKey(userID)) {
+    if (userTrees.containsKey(userID)) { // TODO: deal with the storage of user-specific kdtrees
       kdtree = userTrees.get(userID);
     } else {
       Recipe rootRecipe = new Recipe("ROOT"); // fake node
       RecipeNode rootNode = new RecipeNode(rootRecipe);
       int dims = 6; // make dims based on user or universal?
       kdtree = new KDTree<RecipeNode>(dims, rootNode);
-      List<RecipeNode> nodes = RecipeDatabase.getRecipeSubset();
+      List<RecipeNode> nodes = RecipeDatabase.getRecipeSubset(); // TODO: make method in RecipeDatabase class
+      nodes.addAll(this.convertRecipesToRecipeNodes(userRecs.get(userID))); // adds user history to nodes list
       kdtree.buildTree(0, nodes);
       userTrees.put(userID, kdtree);
     }
     return kdtree;
+  }
+  
+  private List<RecipeNode> convertRecipesToRecipeNodes(List<Recipe> recipes) {
+    List<RecipeNode> nodes = new ArrayList<RecipeNode>();
+    for (Recipe recipe: recipes) {
+      nodes.add(new RecipeNode(recipe));
+    }
+    return nodes;
   }
 
   private List<Recipe> getRecommendedRecipes(KDTree<RecipeNode> kdtree) {
