55b483670db53b26a205aec7d5d073cf3dd34d10
Morgann Thain <morgann.thain@gmail.com>
2020-04-17 19:59:44 -0400

    starting using hasing and salting login info storing method; fixed errors in recommendation from changing kdtree

--- src/main/java/edu/brown/cs/student/login/Accounts.java
+++ src/main/java/edu/brown/cs/student/login/Accounts.java
@@ -9,18 +9,40 @@ import java.util.Scanner;
  * Class comment.
  *
  */
-public class Accounts {
-  private File users;
-  
-  
-  public Accounts(File users) {
-    this.users = users;
+public final class Accounts {
+  private static final String loginInfoFile = "data/account-login-info.csv";
+  private static Scanner loginInfo;
+
+  static {
+    try {
+      loginInfo = new Scanner(new File(loginInfoFile));
+    } catch (FileNotFoundException e) { //TODO: is this okay?
+      System.err.println(e.getCause());
+      System.exit(1);
+    }
+  }
+
+  /**
+   * constructor.
+   * @param loginInfo - File with login information
+   * @throws LoginException - if the file doesn't exist
+   */
+  private Accounts(File loginInfo) throws LoginException {
+    try {
+      this.loginInfo = new Scanner(loginInfo);
+    } catch (FileNotFoundException e) {
+      throw new LoginException("ERROR: checkLogin: account user/pass file not found", e);
+    }
+  }
+
+  public void initializeAccounts() {
+
   }
   // Store w/cookies, database, or hashmap
   
   // Validate method, which affirms that the given password and username are valid and match.
   // https://stackoverflow.com/questions/16627910/how-to-code-a-very-simple-login-system-with-java
-  public String naiveCheckLogin() throws LoginException {
+  public static String naiveCheckLogin() throws LoginException {
     Scanner keyboard = new Scanner (System.in);
     // get input from user
     System.out.println("Username: ");
@@ -28,19 +50,17 @@ public class Accounts {
     System.out.println("Password: ");
     String inpPass = keyboard.nextLine();
     Scanner scan;
-    try {
-      scan = new Scanner(new File("the\\dir\\myFile.extension"));
-    } catch (FileNotFoundException e) {
-      throw new LoginException("ERROR: checkLogin: account user/pass file not found", e);
-    }
 
-    String[] account;
+
+    String[] login;
     String user;
     String pass;
-    while (scan.hasNext()) {
-      account = scan.nextLine().split(",");
-      user = account[0];
-      pass = account[1];
+    String salt;
+    while (Accounts.loginInfo.hasNext()) {
+      login = Accounts.loginInfo.nextLine().split(",");
+      user = login[0];
+      pass = login[1];
+      salt = login[2];
 
       if (inpUser.equals(user) && inpPass.equals(pass)) {
         return login(user, pass);
@@ -52,7 +72,7 @@ public class Accounts {
     throw new LoginException("ERROR: reached end of checkLogin");
   }
 
-  public String login(String user, String pass) {
+  public static String login(String user, String pass) {
     // give access to data of user for future commands
     return "";
   }
---
+++ src/main/java/edu/brown/cs/student/recommendation/RecommendationException.java
@@ -0,0 +1,14 @@
+package edu.brown.cs.student.recommendation;
+
+/**
+ * Recommendation Exception class.
+ */
+public class RecommendationException extends Throwable {
+  /**
+   * constructor.
+   * @param message message to give
+   */
+  public RecommendationException(String message) {
+    super(message);
+  }
+}
