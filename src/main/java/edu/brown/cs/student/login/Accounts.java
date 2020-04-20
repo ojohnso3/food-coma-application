a6a4bc8f02e6dd4b7c89b27885d1339e486837ac
Morgann Thain <morgann.thain@gmail.com>
2020-04-20 15:04:36 -0400

    fixed errors in act and tst

--- src/main/java/edu/brown/cs/student/login/Accounts.java
+++ src/main/java/edu/brown/cs/student/login/Accounts.java
@@ -4,12 +4,16 @@ import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 //import org.springframework.security.crypto.password;
 //import org.springframework.security.crypto.bcrypt.BCrypt;
-import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
@@ -26,18 +30,30 @@ public final class Accounts {
 
   //TODO: is this okay?
   private static final String LOGIN_INFO_PATH = "src/main/resources/login/account-login-info.csv";
-  private static File loginInfoFile =
-          new File(Objects.requireNonNull(
-                  Accounts.class.getClassLoader().getResource(LOGIN_INFO_PATH)).getFile());
-  private static Scanner loginInfo;
+//  private static File loginInfoFile;
+//  private static Scanner loginInfo;
+//  private static PrintWriter writer;
+//
+//  static {
+//    try (Scanner loginInfo = new Scanner(new FileReader(LOGIN_INFO_PATH));
+//         PrintWriter writer = new PrintWriter(new File(LOGIN_INFO_PATH))) { } catch (FileNotFoundException e) {
+//      e.printStackTrace();
+//    }
 
-  static {
-    try {
-      loginInfo = new Scanner(loginInfoFile);
-    } catch (FileNotFoundException e) {
-      System.err.println("login info file was not found");
-    }
-  }
+//    try {
+//      loginInfo = new Scanner(loginInfoFile);
+//    } catch (FileNotFoundException e) {
+//      System.err.println(e.getStackTrace());
+//      System.err.println("login info file was not found");
+//    }
+//
+//    try {
+//      writer = new PrintWriter(Accounts.getLoginInfoFile());
+//    } catch (FileNotFoundException e) {
+//      System.err.println(e.getStackTrace());
+//      System.err.println("login info file corrupted");
+//    }
+//  }
   // Accounts.class.getClassLoader().getResource(loginInfoPath).getFile()
   // Accounts.class.getResourceAsStream(loginInfoPath)
   // new File(loginInfoFile)
@@ -47,8 +63,22 @@ public final class Accounts {
    */
   private Accounts() { }
 
-  protected static File getLoginInfoFile() {
-    return loginInfoFile;
+//  protected static File getLoginInfoFile() {
+//    return loginInfoFile;
+//  }
+
+  public static String readHeader() throws AccountException {
+    try (Scanner loginInfo = new Scanner(new FileReader(LOGIN_INFO_PATH))) {
+      if (loginInfo.hasNext()) {
+        return loginInfo.next();
+      } else {
+        throw new AccountException("header of login info file does not exist");
+      }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+
+    throw new AccountException("login info file does not exist");
   }
 
   // handles salt-hashing for storing password when creating users
@@ -62,6 +92,23 @@ public final class Accounts {
     byte[] salt = new byte[SALT_LENGTH];
     RANDOM.nextBytes(salt);
     return salt;
+  }
+
+  public static byte[] hashPassword(String rawPassword, byte[] salt) {
+    return simpleHash(rawPassword, salt);
+  }
+
+  /**
+   * basic hash coding algorithm. mainly for testing; not necessarily secure.
+   * @param rawPassword pass
+   * @param salt salt
+   * @return hash as String
+   */
+  public static byte[] simpleHash(String rawPassword, byte[] salt) {
+    int res = 0;
+    res = res * 31 + rawPassword.hashCode();
+    res = res * 31 + salt.hashCode();
+    return null;
   }
 
   /**
@@ -100,33 +147,47 @@ public final class Accounts {
 
 
   // Store w/cookies, database, or hashmap
+
+  protected static void writeLoginInfo(String user, String pass) throws AccountException {
+    try (PrintWriter writer = new PrintWriter(new FileWriter(LOGIN_INFO_PATH))) {
+      byte[] salt = generateSalt();
+      byte[] hash = hashPassword(pass, salt);
+      writer.write(user + "," + hash + "," + salt);
+    } catch (IOException e) {
+      throw new AccountException(e.getMessage(), e);
+    }
+  }
   
   // Validate method, which affirms that the given password and username are valid and match.
   // https://stackoverflow.com/questions/16627910/how-to-code-a-very-simple-login-system-with-java
   public static String checkLogin() throws AccountException {
-    Scanner keyboard = new Scanner (System.in);
-    // get input from user
-    System.out.println("Username: ");
-    String inpUser = keyboard.nextLine();
-    System.out.println("Password: ");
-    String inpPass = keyboard.nextLine();
+    try (Scanner keyboard = new Scanner(System.in);
+         Scanner loginInfo = new Scanner(new FileReader(LOGIN_INFO_PATH))) {
+      // get input from user
+      System.out.println("Username: ");
+      String inpUser = keyboard.nextLine();
+      System.out.println("Password: ");
+      String inpPass = keyboard.nextLine();
 
-    String[] login;
-    String user;
-    String passHash;
-    byte[] salt;
-    while (Accounts.loginInfo.hasNext()) {
-      login = Accounts.loginInfo.nextLine().split(",");
-      user = login[0];
-      passHash = login[1];
-      salt = login[2].getBytes();
-      System.out.println(salt); //TODO:
+      String[] login;
+      String user;
+      String passHash;
+      byte[] salt;
 
-      if (user.equals(inpUser) && passHash.equals(hashPasswordPBKDF2(inpPass, salt))) {
-        return login(user, passHash);
-      } else {
-        return "login failed; please try again";
+      while (loginInfo.hasNext()) {
+        login = loginInfo.nextLine().split(",");
+        user = login[0];
+        passHash = login[1];
+        salt = login[2].getBytes();
+
+        if (user.equals(inpUser) && passHash.equals(hashPasswordPBKDF2(inpPass, salt))) {
+          return login(user, passHash);
+        } else {
+          return "login failed; please try again";
+        }
       }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
     }
 
     throw new AccountException("ERROR: reached end of checkLogin");
