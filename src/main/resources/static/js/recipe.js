bd1afa9bbdb2644a6031cadd673f8167a8f6d7dd
cameronwenzel <55778153+cameronwenzel@users.noreply.github.com>
2020-04-25 16:05:12 -0400

    Added ingredients and changed method of posting recommended recipes to recipe page

--- src/main/resources/static/js/recipe.js
+++ src/main/resources/static/js/recipe.js
@@ -25,13 +25,18 @@ function getRecipes(params){
     return $.post("/recipe/recipeuri", params, response =>{
         let obj = JSON.parse(response);
         document.getElementById("title").innerHTML = obj.title;
-        $.each(obj.recipeList, function printRecipe(index, key){
+        $.each(obj.recipeList, function printRecipe(key, value){
             console.log("HELLO???");
-            console.log(this);
-            console.log(index + ": " + key);
-            document.getElementById("recipes").innerHTML += " <h6 id=\"recipes\"><a href=\"/recipe/" + index + "\"> " + key + " </a></h6>";
+
+            console.log(key + ": " + value[0]);
+            document.getElementById("recipes").innerHTML += " <h6 id=\"recipes\"><a href=\"/recipe/" + key + "\"> " + value[0] + " </a></h6>";
 
         })
+        for(let i = 0; i < obj.ingredients.length; i ++){
+            document.getElementById("ingredients").innerHTML += obj.ingredients[i] + "</br>";
+            console.log(obj.ingredients[i]);
+        }
+            // console.log(ingredients(key));
 
     });
 };
