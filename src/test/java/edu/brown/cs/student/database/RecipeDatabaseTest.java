28a5668a1c90676835b5be007d2362f6f1280492
akstokely <annabeth.stokely@gmail.com>
2020-05-04 22:36:38 -0400

    Fixing RecipeDatabaseTest

--- src/test/java/edu/brown/cs/student/database/RecipeDatabaseTest.java
+++ src/test/java/edu/brown/cs/student/database/RecipeDatabaseTest.java
@@ -33,12 +33,15 @@ public class RecipeDatabaseTest {
   @Test
   public void testAll() {
 
-<<<<<<< HEAD
     Map<String, double[]> nutrients = new HashMap<>();
     double[] testVal = new double[2];
     testVal[0] = 1.0;
     testVal[1] = 2.0;
     nutrients.put("CA", testVal);
+
+    List<Ingredient> ingredients = new ArrayList<>();
+    Ingredient i = new Ingredient("text", 0.0);
+    ingredients.add(i);
 
     List<String> labels = new ArrayList<>();
     labels.add("a");
@@ -51,26 +54,7 @@ public class RecipeDatabaseTest {
     int num = rand.nextInt(1000);
     this.r3 = new Recipe("http://edamam.api.com/Ontology#" + num, "label", "image" , "source",
         "url", 0.0, 0.0, 0.0, 0.0, ingredients, nutrients, labels, labels);
-=======
-//    List<Ingredient> ingredients = new ArrayList<>();
-//    Ingredient i = new Ingredient("text", 0.0);
-//    ingredients.add(i);
-//
-//    Map<String, double[]> nutrients = new HashMap<>();
-//    double[] testVal = new double[2];
-//    testVal[0] = 1.0;
-//    testVal[1] = 2.0;
-//    nutrients.put("CA", testVal);
-//
-//    Random rand = new Random();
-//    String uri = rand.nextInt(1000) + "";
-//    this.r2 = new Recipe(uri, "label", "image", "source", "url", 0.0,
-//        0.0, 0.0, 0.0, ingredients, nutrients);
-//
-//    int num = rand.nextInt(1000);
-//    this.r3 = new Recipe("http://edamam.api.com/Ontology#" + num, "label", "image" , "source",
-//        "url", 0.0, 0.0, 0.0, 0.0, ingredients, nutrients);
->>>>>>> e5ba69571543c97870dd9bca47137489f5f2b61c
+
 
     this.testInsertRecipe(uri, num);
     this.testGetRecipeFromUri();
